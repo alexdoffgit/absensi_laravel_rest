fe8fc25006ee746e64971a82501279a9587cd3ea
miridium-electrode <checkerdance@yahoo.com>
2024-05-31 21:38:54 +0700

    add presence page

---
+++ app/Http/Controllers/Kehadiran.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\Karyawan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Kehadiran extends Controller
+{
+    public function __construct(private Karyawan $store) {}
+
+    public function index(Request $request, $uid) 
+    {
+        $positionTableData = DB::table('temp_user_jabatan')
+            ->select('jabatan')
+            ->where('userid', '=', intval($uid))
+            ->first();
+        $presenceTableData = $this->store->getPresensi(intval($uid));
+        $absenceTableData = $this->store->getAbsensi(intval($uid));
+
+
+        return view('kehadiran', [
+            'presenceTableData' => $presenceTableData,
+            'absenceTableData' => $absenceTableData,
+            'position' => $positionTableData->jabatan,
+            'karyawanId' => $uid
+        ]);
+    }
+}
