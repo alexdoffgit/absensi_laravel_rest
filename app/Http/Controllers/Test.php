88704c185c406e387677a152777deef722b9633b
miridium-electrode <checkerdance@yahoo.com>
2024-06-18 12:51:45 +0700

    add testing config

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -11,8 +11,18 @@ class Test extends Controller
     {
         // select checktime, checktype, userid dalam  5 tahun dari hari ini
         // pisahkan menjadi format date, time start, time end
-        
+        $this->testing_how_long_i_can_go_back_in_time_before_memory_run_out();
 
         return view('test', ['jabatan' => 'hr', 'uid' => 3]);
+    }
+
+    private function testing_how_long_i_can_go_back_in_time_before_memory_run_out()
+    {
+        $checkinoutTable = DB::table('checkinout')
+            ->whereDate('CHECKTIME', '>=', '2021-12-01')
+            ->select(['CHECKTYPE', 'CHECKTIME'])
+            ->get();
+
+        dd($checkinoutTable);
     }
 }
