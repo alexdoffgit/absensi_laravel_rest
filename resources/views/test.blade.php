7ba371fc433890717b60a7c2c6518d08aec53cde
miridium-electrode <checkerdance@yahoo.com>
2024-07-21 19:04:29 +0700

    test: layout

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,30 +8,7 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    {{-- <div class="container-fluid">
-        <div class="row">
-            <div class="col-2 bg-black d-flex flex-column" style="height: 100vh">
-                @foreach ($menus as $menu)
-                    @if (count($menu->menu_name) > 1)
-                        @php
-                            $offset = 0;
-                            $lastLink = count($menu->menu_name) - 1;
-                        @endphp
-                        @foreach ($menu->menu_name as $i => $menuName)
-                            @if ($i == $lastLink)
-                                <a class="text-white ms-menu-{{ $offset }}" href="{{ action([$menu->laravel_controller_class, $menu->laravel_controller_method], ['uid' => 1]) }}" style="text-decoration: none;">
-                                    {{ $menuName }}
-                                </a>    
-                            @else
-                                <div class="text-white ms-menu-{{ $offset }}">{{ $menuName }}</div>
-                            @endif
-                            @php($offset += 1)
-                        @endforeach
-                    @endif
-                @endforeach
-            </div>
-        </div>
-    </div> --}}
-    <x-sidenav />
+    {{-- <x-sidenav /> --}}
+    <x-layout title="test"/>
 </body>
 </html>
