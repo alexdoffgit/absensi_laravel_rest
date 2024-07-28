5ee447d70abacb954e182653555ee1c28c2605bf
miridium-electrode <checkerdance@yahoo.com>
2024-07-29 06:08:51 +0700

    test: use dummy data for calendar

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,23 @@ Route::prefix('test')->group(function () {
     Route::get('/', [Test::class, 'index']);
 });
 
-$menuTable = DB::table('menu_links as ml')
-    ->whereNotNull('ml.laravel_controller_class')
-    ->whereNotNull('ml.laravel_controller_method')
-    ->whereNotNull('ml.http_method')
-    ->where('ml.route_type', '=', 'web')
-    ->select([
-        'ml.id',
-        'ml.menu_path',
-        'ml.laravel_controller_class',
-        'ml.laravel_controller_method',
-        'ml.laravel_middleware',
-        'ml.http_method'
-    ])
-    ->get();
+// $menuTable = DB::table('menu_links as ml')
+//     ->whereNotNull('ml.laravel_controller_class')
+//     ->whereNotNull('ml.laravel_controller_method')
+//     ->whereNotNull('ml.http_method')
+//     ->where('ml.route_type', '=', 'web')
+//     ->select([
+//         'ml.id',
+//         'ml.menu_path',
+//         'ml.laravel_controller_class',
+//         'ml.laravel_controller_method',
+//         'ml.laravel_middleware',
+//         'ml.http_method'
+//     ])
+//     ->get();
+
+$menu = new App\Repository\Json\Menu();
+$menuTable = $menu->webLinks();
 
 foreach ($menuTable as $menuRow) {
     if (!empty($menuRow->laravel_middleware)) {
