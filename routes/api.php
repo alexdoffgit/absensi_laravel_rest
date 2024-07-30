02bcee22cfefeaeb7834a40d7790b3fc481bcffd
miridium-electrode <checkerdance@yahoo.com>
2024-07-30 14:14:18 +0700

    fix: disable api routes for test

--- routes/api.php
+++ routes/api.php
@@ -8,34 +8,34 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-$routes = DB::table('menu_links as ml')
-    ->whereNotNull('ml.laravel_controller_class')
-    ->whereNotNull('ml.laravel_controller_method')
-    ->whereNotNull('ml.http_method')
-    ->where('ml.route_type', '=', 'api')
-    ->select([
-        'ml.id',
-        'ml.menu_path',
-        'ml.laravel_controller_class',
-        'ml.laravel_controller_method',
-        'ml.laravel_middleware',
-        'ml.http_method'
-    ])
-    ->get();
+// $routes = DB::table('menu_links as ml')
+//     ->whereNotNull('ml.laravel_controller_class')
+//     ->whereNotNull('ml.laravel_controller_method')
+//     ->whereNotNull('ml.http_method')
+//     ->where('ml.route_type', '=', 'api')
+//     ->select([
+//         'ml.id',
+//         'ml.menu_path',
+//         'ml.laravel_controller_class',
+//         'ml.laravel_controller_method',
+//         'ml.laravel_middleware',
+//         'ml.http_method'
+//     ])
+//     ->get();
 
-foreach ($routes as $value) {
-    if (!empty($value->laravel_middleware)) {
-        Route::match(
-            $value->http_method, 
-            $value->menu_path, 
-            [$value->laravel_controller_class, $value->laravel_controller_method]
-        )
-        ->middleware($value->laravel_middleware);
-    } else {
-        Route::match(
-            $value->http_method, 
-            $value->menu_path, 
-            [$value->laravel_controller_class, $value->laravel_controller_method]
-        );
-    }
-}
+// foreach ($routes as $value) {
+//     if (!empty($value->laravel_middleware)) {
+//         Route::match(
+//             $value->http_method, 
+//             $value->menu_path, 
+//             [$value->laravel_controller_class, $value->laravel_controller_method]
+//         )
+//         ->middleware($value->laravel_middleware);
+//     } else {
+//         Route::match(
+//             $value->http_method, 
+//             $value->menu_path, 
+//             [$value->laravel_controller_class, $value->laravel_controller_method]
+//         );
+//     }
+// }
