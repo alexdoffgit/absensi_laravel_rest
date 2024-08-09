cb4ee732098c5947ffd305711a26a2c9f0eb02d0
miridium-electrode <checkerdance@yahoo.com>
2024-08-09 17:34:59 +0700

    update leave submission translation

--- lang/en/leave-submission.php
+++ lang/en/leave-submission.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'invalid-date-start' => 'date start must not be less than 3 days from today'
+    'invalid-date-start' => 'date start must not be less than 3 days from today',
+    'invalid-date-range' => 'date range is invalid',
+    'request-date' => 'Request Date',
+    'leave-period' => 'Leave Period'
 ];
--- lang/id/leave-submission.php
+++ lang/id/leave-submission.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'invalid-date-start' => 'hari mulai harus lebih 3 hari dari hari ini'
+    'invalid-date-start' => 'hari mulai harus lebih 3 hari dari hari ini',
+    'invalid-date-range' => 'jangka waktu tidak valid',
+    'request-date' => 'Tanggal Pengajuan',
+    'leave-period' => 'Jangka Waktu Cuti'
 ];
