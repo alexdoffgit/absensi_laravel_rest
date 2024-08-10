40e305423efe5fe8a1e12ea2d70fea5a5051eb0d
miridium-electrode <checkerdance@yahoo.com>
2024-08-10 21:15:03 +0700

    fix: change wrong table name in down migration

--- database/migrations/2024_07_30_004610_create_leave_allowances_table.php
+++ database/migrations/2024_07_30_004610_create_leave_allowances_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('leave_allowance');
+        Schema::dropIfExists('leave_allowances');
     }
 };
