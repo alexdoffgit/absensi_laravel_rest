ca60680d3fd0e03afba02ca677db1b58c6ca48ac
miridium-electrode <checkerdance@yahoo.com>
2024-08-11 19:01:34 +0700

    chore: add relation between user and department

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -54,5 +55,10 @@ class User extends Authenticatable
     public function pivot_vice_manager(): HasOne
     {
         return $this->hasOne(UserManager::class, 'vice_manager_id', 'USERID');
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class, 'DEFAULTDEPTID', 'DEPTID');
     }
 }
