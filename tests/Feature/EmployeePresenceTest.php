7d92c049972dbcfd991db19323bbd092440cddd7
miridium-electrode <checkerdance@yahoo.com>
2024-08-25 11:33:36 +0700

    chore: create test for employee presence

---
+++ tests/Feature/EmployeePresenceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Repository\Database\Attendance;
+
+class EmployeePresenceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        
+    }
+}
